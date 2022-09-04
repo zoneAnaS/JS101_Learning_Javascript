9ed1e6ac8625b6160c0dec027424c3ba35e0a6cd
zoneAnaS <shaikhanas4321@gmail.com>
2022-09-04 13:04:43 +0530

    Javascript probelm day 1

---
+++ Assignment31082022/problem2.js
@@ -0,0 +1,6 @@
+let name = "Anas Shaikh";
+console.log(name);
+ name = "Aminuddin Shaikh";
+console.log(name);
+ name = "Salma Shaikh";
+console.log(name);
---
+++ Assignment31082022/problem3.js
@@ -0,0 +1,5 @@
+let name = "Anas Shaikh";
+let age = 20;
+
+console.log(name,age);
+console.log(typeof(name),typeof(age));
