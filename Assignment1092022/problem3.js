bc2972412fa7b82bc39a6ce028fd2e60c87545e2
zoneAnaS <shaikhanas4321@gmail.com>
2022-09-04 13:12:49 +0530

    assignment day 2

---
+++ Assignment1092022/problem3.js
@@ -0,0 +1,11 @@
+let one = 1;
+let two = 2;
+let three = 3;
+let four =4;
+let five = 5;
+let six = 6;
+
+
+let double = (two+five+six)*2;
+let expo = (three*3)+(four*3);
+console.log(one+double+expo);
